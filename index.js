5f126028ea9e08eac8f7021b400af83733dee50d
Aboubacar <minonaboubacars@gmail.com>
2023-07-18 18:51:32 +0000

    Enreg_2

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
+// alert('Le fichier fonctionne');
+
+
+
+
+// console.log("Hello");
+
+// let maVarriableSuperCool = "kamelCase";
+
+// console.log(maVarriableSuperCool);
+
+
+
+
+
+
+
+// let unChiffre = 24;
+
+// unChiffre = 15;
+
+// console.log(unChiffre);
+
+
+
+// let chaine = "je suis une chaine de caractère*"
+
+
+// let chaine = "15*"
+
+// let nouvelleChaine = "Chaine Précédente : " + chaine;
+
+// console.log(nouvelleChaine);
+
+
+
+
+
+// let Chiffre = "25*"
+
+// let nouvelleChaine = 'le chiffre attendu est : " &{chiffre} degrés' ;
+
+// console.log(nouvelleChaine);
+
+
+// let string = "je suis une chaine";
+// let number = 24;
+// let booleen =["je", "suis", 24, false];
+// let array = ["je", "suis", 24, false];
+
+// let object = {
+//     prenon: "Aboubacar",
+//     age: 34,
+//     ville: "Bordeaux",
+// };
+
+// console.log(typeof array);
+
+
+
+let arbre;
+
+console.log(arbre)
+
+  
+
+
+
+ 
+ 
+
+
+
+
 let total = 0;
 
 function addition(x) {
